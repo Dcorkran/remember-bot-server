644fcb15ce6cd7ae78e28600f0ee220d04b76025
Dillon Corkran <dcorkran972@gmail.com>
2017-04-18 15:57:01 -0600

    :+1: update seeds

--- seeds/01.js
+++ seeds/01.js
@@ -7,7 +7,9 @@ exports.seed = function(knex, Promise) {
       return knex('name').insert([
         {id: 1, name: 'Dillon', description: 'Cool guy'},
         {id: 2, name: 'Dylan', description: 'Cool guy'},
-        {id: 3, name: 'Dillion', description: 'Cool guy'}
+        {id: 3, name: 'Dillion', description: 'Cool guy'},
+        {id: 4, name: 'Cole', description: 'Cole is a really cool guy'},
+        {id: 5, name: 'James', description: 'James is really good at Career Services but not Cornhole'}
       ]);
     });
 };
